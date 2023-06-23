bcd9df1bbcb769ff29315f7dc75c6b09d2a7e143
wn1331 <wn1331@gmail.com>
2023-06-23 13:23:15 +0900

    스프링 컨테이너로 싱글톤 구현

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -1,10 +1,14 @@
 package hello.core.singleton;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import hello.core.config.AppConfig;
 import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -23,7 +27,38 @@ public class SingletonTest {
         System.out.println("memberService2: " + memberService2);
 
         //3. memberservice1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println(singletonService1+" //// "+singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //isSameAs와 isEqualTo의 차이 ---
+        //isSameAs -> 객체 자체를 비교
+        //isEqualTo -> 객체의 값만 비교
+
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println(memberService1+" //// "+memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
+
     }
 
 }
